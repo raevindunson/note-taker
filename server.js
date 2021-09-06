a834a5ad6bb259317b991b25f264060c430141cd
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-05 23:01:03 -0400

    server js updates

---
+++ server.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express()
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+//Got help from https://github.com/kara-krzystan/note-taker
