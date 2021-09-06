13195e8300c845cf054955a2e15eacd4b584f48b
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-05 23:03:08 -0400

    server updates

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express()
 
 app.use(express.json());
